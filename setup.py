6d65f4e369d7732e7a2eeff5a42316bc504ef689
DmytroLievoshko <levka296@gmail.com>
2022-12-11 19:13:19 +0200

    change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(name='pyCliAddressBook',
       version='1.0.22',
       description='Personal assistant with command line interface',
-      url='https://github.com/yuragoit/pyCliAddressBook',
+      url='',
       author='Yurii Skiter, Valerii Sydorenko, Dmytro Levoshko',
       author_email='contract@restriction.com',
       license='MIT',
