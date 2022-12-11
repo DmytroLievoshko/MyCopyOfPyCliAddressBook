a897cd9f468118551ec89a639b501847d00320a2
DmytroLievoshko <levka296@gmail.com>
2022-12-11 19:14:25 +0200

    url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(name='pyCliAddressBook',
       version='1.0.22',
       description='Personal assistant with command line interface',
-      url='',
+      url='https://github.com/DmytroLievoshko/MyCopyOfPyCliAddressBook.git',
       author='Yurii Skiter, Valerii Sydorenko, Dmytro Levoshko',
       author_email='contract@restriction.com',
       license='MIT',
